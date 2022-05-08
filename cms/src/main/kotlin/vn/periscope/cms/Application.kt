3fb38f784ac678998ad72f33aaa0963509728554
Hieu Tran <hieutran.fu@gmail.com>
2022-05-08 17:28:04 +0700

    update ktor version

--- cms/src/main/kotlin/vn/periscope/cms/Application.kt
+++ cms/src/main/kotlin/vn/periscope/cms/Application.kt
@@ -5,16 +5,17 @@ import org.koin.logger.SLF4JLogger
 import org.koin.dsl.module
 import vn.periscope.cms.adapters.adapterModule
 import vn.periscope.cms.core.coreModule
-import vn.periscope.cms.plugins.KoinPlugin
+import vn.periscope.cms.plugins.Koin
+
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    install(KoinPlugin) {
+    install(Koin) {
         SLF4JLogger()
-        modules(
+        modules = arrayListOf(
             module {
                 single { this@module }
             },
